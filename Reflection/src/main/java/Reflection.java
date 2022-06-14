999a837ffcc50564754ec2bc148ca9deef3eb1a1
kushevsky01 <78921029+kushevsky01@users.noreply.github.com>
2022-06-14 13:17:28 +0300

    Add files via upload

--- Reflection/src/main/java/Reflection.java
+++ Reflection/src/main/java/Reflection.java
@@ -4,79 +4,119 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.SocketTimeoutException;
 
-class UtilClass{
+class UtilClass {
 
     private final Class cls;
 
-    UtilClass(Class cls){
+    UtilClass(Class cls) {
         this.cls = cls;
     }
 
-    public void showInfo(){
-        System.out.println("Class name :"+ cls.getName());
+    public void showInfo() {
+        clasaName(cls);
 
+        packageName(cls);
+
+        interfaceName(cls);
+
+        superClass(cls);
+
+        constuctors(cls);
+
+        fields(cls);
+
+        methods(cls);
+    }
+
+    public void clasaName(Class cls) {
+        System.out.println("Class name :" + cls.getName());
+    }
+
+    public void packageName(Class cls) {
         System.out.println("Package :" + cls.getPackageName());
         System.out.println("");
-        Class<?>[] inte = cls.getInterfaces();
+    }
 
+    public void interfaceName(Class cls) {
+        Class<?>[] inte = cls.getInterfaces();
         System.out.println();
         System.out.println("Interface");
-        for(Class in: inte ){
+
+
+        for (Class in : inte) {
             System.out.println(in);
         }
+        System.out.println("");
+    }
 
+    public void superClass(Class cls) {
         System.out.println("Super class");
         Class cl = cls.getSuperclass();
         System.out.println(cl.getName());
+    }
 
+    public void constuctors(Class cls) {
         Constructor[] cons = cls.getConstructors();
-
         System.out.println("Constructors:");
         System.out.println(" ");
-        for (Constructor con : cons){
 
+
+        for (Constructor con : cons) {
             System.out.println("Constructor");
             System.out.println(con.getName());
-
             Class[] types = con.getParameterTypes();
-            for (Class ct: types){
 
+
+            for (Class ct : types) {
                 System.out.println("Constructor parameter types");
-                System.out.println("- "+ ct.getName());
+                System.out.println("- " + ct.getName());
                 System.out.println("");
             }
 
 
         }
+    }
 
+    public void fields(Class cls){
         Field[] fld = cls.getDeclaredFields();
-        for (Field fl : fld){
+
+
+        for (Field fl : fld) {
             System.out.println("");
             System.out.println("Fields: ");
-            System.out.println("Var :" + fl.getName()+"("+fl.getType().getName()+")");
+            System.out.println("Var :" + fl.getName() + "(" + fl.getType().getName() + ")");
         }
+    }
+
+    public void methods(Class cls){
         Method[] method = cls.getMethods();
         System.out.println("");
-        System.out.println("There are "+method.length+" methods :");
-        for (Method meth : method) {
+        System.out.println("There are " + method.length + " methods :");
 
-            System.out.println("Mehod name: "+meth.getName());
+
+        for (Method meth : method) {
+            System.out.println("");
+            System.out.println("Mehod name: " + meth.getName());
             System.out.println("Return type : " + meth.getReturnType().getName());
             Class<?>[] params = meth.getParameterTypes();
             System.out.print("Parameters : ");
+
+
             for (Class<?> param : params)
                 System.out.print(" " + param.getName());
+
+
             Annotation[] ann = meth.getAnnotations();
             for (Annotation an : ann) {
                 System.out.println("Annotation");
                 System.out.println(an.annotationType());
+
             }
-            System.out.println("");
+
         }
-
-
-
     }
+
+
 }
 
 public class Reflection {
